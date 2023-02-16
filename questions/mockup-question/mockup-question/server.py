84544c9dcbf0864c6ef0ee6e7a60289cacb33972
Bowen Fan <bowenfan1@berkeley.edu>
2023-02-16 00:35:10 -0800

    Created outline for mockup question

--- questions/mockup-question/mockup-question/server.py
+++ questions/mockup-question/mockup-question/server.py
@@ -1,18 +1,18 @@
-# AUTO-GENERATED FILE
-# go to https://prairielearn.readthedocs.io/en/latest/python-grader/#serverpy for more info
+import random
+
 
 def generate(data):
-    # Define incoming variables here
-    names_for_user = [
-        # ex: student receives a matrix m
-        # {"name": "m", "description": "a 2x2 matrix", "type": "numpy array"}
-    ]
-    # Define outgoing variables here
-    names_from_user = [
-        {"name": "a", "description": "", "type": "python var"},
-    ]
 
-    data["params"]["names_for_user"] = names_for_user
-    data["params"]["names_from_user"] = names_from_user
+    # Sample two random integers between 5 and 10 (inclusive)
+    a = random.randint(5, 10)
+    b = random.randint(5, 10)
 
-    return data
+    # Put these two integers into data['params']
+    data["params"]["a"] = a
+    data["params"]["b"] = b
+
+    # Compute the sum of these two integers
+    c = a + b
+
+    # Put the sum into data['correct_answers']
+    data["correct_answers"]["c"] = c
